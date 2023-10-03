b90c2dc72c4466850f8839fb4e724c6c7a836d32
Febisalsa723 <124450613+Febisalsa723@users.noreply.github.com>
2023-10-03 23:23:34 +0700

    Add files via upload

---
+++ Tugas3.js
@@ -0,0 +1,10 @@
+// no 1 buatlah variabel firstName, lastName, age, isMerried dengan ketentuan 
+var firstName = " febi ";
+var lastName = " azali ";
+var age = 20;
+var ismarried = false;
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(ismarried)
